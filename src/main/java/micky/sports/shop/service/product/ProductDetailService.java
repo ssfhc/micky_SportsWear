1a7853ae506bce700dfeb725a5434176a6626022
mhwa9191 <mmhhwa@gmail.com>
2023-02-24 15:01:11 +0900

    하단리뷰확인1차

--- src/main/java/micky/sports/shop/service/product/ProductDetailService.java
+++ src/main/java/micky/sports/shop/service/product/ProductDetailService.java
@@ -1,7 +1,9 @@
 package micky.sports.shop.service.product;
 
+import java.util.ArrayList;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -9,6 +11,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.ui.Model;
 
 import micky.sports.shop.dao.ProductDao;
+import micky.sports.shop.dao.ReviewDao;
+import micky.sports.shop.dto.ReviewDto;
 import micky.sports.shop.service.MickyServiceInter;
 
 public class ProductDetailService implements MickyServiceInter{
@@ -37,7 +41,54 @@ public class ProductDetailService implements MickyServiceInter{
 		ProductDao Pdao=sqlSession.getMapper(ProductDao.class);
 		model.addAttribute("productMain",Pdao.productMain(pname));
 		model.addAttribute("product",Pdao.product(pname,pfilesrc));
+		System.out.println("*pname : "+pname);
+		System.out.println("*pfilesrc : "+pfilesrc);
 		
+		//하단리뷰
+		ServletContext application=request.getSession().getServletContext();
+		String p_name=pname;
+		String p_filesrc=pfilesrc;
+		
+		System.out.println("pname : "+p_name);
+		System.out.println("pfilesrc : "+p_filesrc);
+		
+		//String r_no=request.getParameter("r_no");
+		
+//		검색기능
+		String selectType=request.getParameter("selectType");
+		
+		String r_group="";
+		String r_score="";
+		
+		if (selectType==null||selectType.equals("")) {
+			selectType="r_group";
+		}
+		
+//		searchKeyword 가져오기
+		String searchKeyword=request.getParameter("searchKeyword");
+		if (searchKeyword==null||searchKeyword.equals("")) {
+			searchKeyword="";
+		}
+		System.out.println("sk : "+searchKeyword);
+
+		ReviewDao rdao=sqlSession.getMapper(ReviewDao.class);
+		
+//		총게시글 수
+		int totalCount=0;
+		totalCount=rdao.totalCount(p_name);
+//		별점평균
+		double avgStarscore=0;
+		avgStarscore=rdao.avgStarscore(p_name);
+
+		ArrayList<ReviewDto> review_list=rdao.reviewboard(selectType,searchKeyword,p_name);
+		
+		model.addAttribute("review_list", review_list);
+//		검색어 유지기능
+		model.addAttribute("resk",searchKeyword);
+//		총게시글
+		model.addAttribute("totalCount", totalCount);
+//		별점평균
+		model.addAttribute("avgStarscore", avgStarscore);
 	}
 
 }
